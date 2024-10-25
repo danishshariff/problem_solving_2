187f9c09789e648f07732df48068bfbc8e71198a
danishshariff <132253347+danishshariff@users.noreply.github.com>
2024-10-25 23:20:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int count(vector<int>& arr, int target) {
+        
+        // code here
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.count(arr, d);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
