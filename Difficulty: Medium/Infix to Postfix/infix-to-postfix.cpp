ad40c5bc533fde2ad7938294f2d88b376bdbc60c
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-02-26 19:52:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -6,66 +6,64 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
-    // Function to convert an infix expression to a postfix expression.
+    // Function to return precedence of operators.
     int prec(char c) {
-  if (c == '^')
-    return 3;
-  else if (c == '/' || c == '*')
-    return 2;
-  else if (c == '+' || c == '-')
-    return 1;
-  else
-    return -1;
-}
+        if (c == '^') return 3;
+        else if (c == '/' || c == '*') return 2;
+        else if (c == '+' || c == '-') return 1;
+        else return -1;
+    }
+
+    // Function to convert infix expression to postfix expression.
     string infixToPostfix(string& s) {
-        // Your code here
-         stack < char > st; //For stack operations, we are using C++ built in stack
-  string result;
+        stack<char> st; // Stack to store operators
+        string result;
 
-  for (int i = 0; i < s.length(); i++) {
-    char c = s[i];
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            // If operand, add to result
+            if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')) {
+                result += s[i];
+            } 
+            // If '(', push to stack
+            else if (s[i] == '(') {
+                st.push(s[i]);
+            } 
+            // If ')', pop until '('
+            else if (s[i] == ')') {
+                while (!st.empty() && st.top() != '(') {
+                    result += st.top();
+                    st.pop();
+                }
+                if (!st.empty()) st.pop(); // Pop '('
+            } 
+            // Operator case
+            else {
+                while (!st.empty() && prec(s[i]) <= prec(st.top())) {
+                    result += st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+            i++;
+        }
 
-    // If the scanned character is
-    // an operand, add it to output string.
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-      result += c;
+        // Pop remaining operators
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
 
-    // If the scanned character is an
-    // ‘(‘, push it to the stack.
-    else if (c == '(')
-      st.push('(');
-
-    // If the scanned character is an ‘)’,
-    // pop and to output string from the stack
-    // until an ‘(‘ is encountered.
-    else if (c == ')') {
-      while (st.top() != '(') {
-        result += st.top();
-        st.pop();
-      }
-      st.pop();
-    }
-
-    //If an operator is scanned
-    else {
-      while (!st.empty() && prec(s[i]) <= prec(st.top())) {
-        result += st.top();
-        st.pop();
-      }
-      st.push(c);
-    }
-  }
-
-  // Pop all the remaining elements from the stack
-  while (!st.empty()) {
-    result += st.top();
-    st.pop();
-  }
-  return result;
+        return result;
     }
 };
+
+
+
 
 
 
