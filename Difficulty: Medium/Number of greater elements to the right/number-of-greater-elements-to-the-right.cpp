0b14cb7b9bdfa9170918a672cd4d08f20c94daef
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-03-03 23:15:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices){
+        //write your code here
+      std::vector<int> result(queries);
+    std::vector<int> nge(n, 0); // To store the count of next greater elements for each element
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] > arr[i]) {
+                nge[i]++;
+            }
+        }
+    }
+    
+    for (int i = 0; i < queries; ++i) {
+        result[i] = nge[indices[i]];
+    }
+    
+    return result;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> v1(n);
+       for(int i = 0;i<n;i++) cin >> v1[i];
+        int q; cin >> q;
+        vector<int> v2(q);
+        for(int i = 0;i<q;i++) cin >> v2[i];
+
+        Solution obj;
+        vector<int> ans = obj.count_NGE(n,v1,q,v2);
+
+        for(int i = 0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << endl;
+
+        
+        // cout << "~\n";
+    
+cout << "~" << "\n";
+}
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
