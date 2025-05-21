6defa2fefa7d21631add82c9a06448bdab38dee7
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-05-21 16:30:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -1,56 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
-    // Function returns the second
-    // largest elements
     int getSecondLargest(vector<int> &arr) {
-        // Code Here]
-        int n=arr.size();
-        int lar=arr[0];
-        int slar=-1;
-        for(int i=1;i<n;i++){
-            if(arr[i]>lar){
-                slar=lar;
-                lar=arr[i];
-                
+        // code here
+        int largest=arr[0];
+        int slarg=-1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>largest){
+                slarg=largest;
+                largest=arr[i];
             }
-            else if(arr[i]<lar && arr[i]>slar){
-                slar=arr[i];
+            else if(arr[i]<largest && arr[i]>slarg){
+                slarg=arr[i];
             }
         }
-        return slar;
-        
+        return slarg;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        int ans = ob.getSecondLargest(arr);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
