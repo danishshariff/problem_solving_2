ecfc3bd3a0abe929429bdd8b7996072db8577e81
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-08-28 16:56:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        // code here
+        if(s1.length()!=s2.length()) return false;
+        int freq[26]={0};
+        for(int i=0;i<s1.length();i++){
+            freq[s1[i]-'a']++;
+            freq[s2[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0) return false;
+        }
+        return true;
+    }
+};
